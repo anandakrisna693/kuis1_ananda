9a6618cb0b0193eec36a42f658af7765fd6d7512
anandakrisna693 <ananda.krisna693@sma.belajar.id>
2026-09-21 11:28:01 +0700

    Update project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,55 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>@yield('title')</title>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f6f8;
+    }
+
+    header {
+        background-color: #123c69;
+        color: white;
+        padding: 20px 8%;
+    }
+
+    .nama-kampus {
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    main {
+        padding: 40px 8%;
+    }
+</style>
 </head>
+
 <body>
 
     <header>
-        <h1>Website Kampus</h1>
-    </header>
+    <div class="header">
+        <div class="nama-kampus">
+            POLINEMA PSDKU PAMEKASAN
+        </div>
+    </div>
+</header>
 
     <main>
         @yield('content')
     </main>
 
 </body>
+
 </html>
